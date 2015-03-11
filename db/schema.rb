2ceea0625d6dec69a89b296f78baed07adba4409
Greg Narajka <gregnar@gmail.com>
2015-03-11 14:43:20 -0600

    add relationships and add user_id to locations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150311202209) do
+ActiveRecord::Schema.define(version: 20150311204035) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 20150311202209) do
     t.text     "clue"
     t.decimal  "latitude"
     t.decimal  "longitude"
+    t.integer  "user_id"
   end
 
   create_table "teams", force: :cascade do |t|
