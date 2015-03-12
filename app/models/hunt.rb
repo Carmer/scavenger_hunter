42206c3757ee592366ba9fa987c23064dad761e3
Greg Narajka <gregnar@gmail.com>
2015-03-12 13:53:06 -0600

    add hunt crud functionality

--- app/models/hunt.rb
+++ app/models/hunt.rb
@@ -1,3 +1,8 @@
 class Hunt < ActiveRecord::Base
   belongs_to :user
+
+  def locations
+    Location.find(master_path)
+  end
+  
 end
--- app/models/team.rb
+++ app/models/team.rb
@@ -1,3 +1,12 @@
 class Team < ActiveRecord::Base
   belongs_to :hunt
+
+  def current_location
+    Location.find(path.first)
+  end
+
+  def complete_location
+    
+  end
+
 end
